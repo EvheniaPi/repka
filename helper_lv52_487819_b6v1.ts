268b2503c4dda7e5a2d8371377b7764fa538870b
EvheniaPi <evheniiap653@gmail.com>
2024-04-18 12:51:51 +0300

    Optimized tests

---
+++ helper_lv52_487819_b6v1.ts
@@ -0,0 +1,9 @@
+const formatDate = date => new Date(date).toLocaleDateString();
+console.log(getRandomString());
+apple - true
+const squareRoot = num => Math.sqrt(num);
+
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+const findSmallestNumber = numbers => Math.min(...numbers);
+90 * false
+const getRandomElement = array => array[getRandomIndex(array)];
